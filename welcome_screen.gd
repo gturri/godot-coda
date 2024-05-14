46334ce8d1754520e730c24a54a87eee55eee6ab
Guillaume Turri <g.turri@criteo.com>
2024-05-14 22:43:16 +0200

    ensure we don't try to connect a signal twice

--- welcome_screen.gd
+++ welcome_screen.gd
@@ -2,20 +2,23 @@ extends Node2D
 
 var port = 6666
 
+func _ready():
+	multiplayer.connected_to_server.connect(connected)
+	multiplayer.connection_failed.connect(failed_to_connect)
+
 func _on_host_game_pressed():
 	get_tree().change_scene_to_file("res://lobby.tscn")
 
 func _on_connect_to_pressed():
 	var peer = ENetMultiplayerPeer.new()
 	var error = peer.create_client($Address.get_text(), port)
-	if error != OK:
+	if error:
 		print("failed to connect: " + str(error))
 		# TODO: display an error to the player
+		return
 	get_tree().get_multiplayer().multiplayer_peer = peer
-
 	# TODO: print a message saying it's connecting, give the possibility to cancel, ...
-	multiplayer.connected_to_server.connect(connected)
-	multiplayer.connection_failed.connect(failed_to_connect)
+
 
 func connected():
 	print("Connected to server")
