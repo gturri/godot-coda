c2bb28989abebca4a412ff9d737fa12ad6397748
Guillaume Turri <g.turri@criteo.com>
2024-05-17 22:40:43 +0200

    Any player can pick a card

--- available_tiles.gd
+++ available_tiles.gd
@@ -5,31 +5,59 @@ const sourceId := 0
 @export var maxValue := 11
 @export var cardsPerRow := 5
 var cards: Array[Card] = []
+signal cardPicked(card: Card)
 
 func _ready():
 	if not multiplayer.is_server():
 		print("We're not the server, we don't create the deck")
 		return
 
-	initialize_cards()
-	print_cards.rpc(CardSerializer.serialize_deck(cards))
+	draw_cards()
+	initialize_cards_remotely.rpc(CardSerializer.serialize_deck(cards))
+	paint()
 
-func initialize_cards() -> void:
+func draw_cards() -> void:
 	for v in maxValue:
 		for c in nbColors:
 			cards.append(Card.new(c, v))
 	cards.shuffle()
 
-@rpc("authority", "call_local", "reliable")
-func print_cards(cardsSeralized: String) -> void:
-	print("received cards to print")
-	var cards = CardSerializer.deserialize_deck(cardsSeralized)
+@rpc("authority", "call_remote", "reliable")
+func initialize_cards_remotely(cardsSerialized: String) -> void:
+	cards = CardSerializer.deserialize_deck(cardsSerialized)
+	paint()
+
+func paint() -> void:
 	for i in cards.size():
-		set_cell(0, cardId_to_cellPos(i), sourceId, card_to_tile(cards[i]))
+		var tile_pos: Vector2i = cardId_to_cellPos(i)
+		if cards[i]:
+			set_cell(0, tile_pos, sourceId, card_to_tile(cards[i]))
+		else:
+			erase_cell(0, tile_pos)
 
 func cardId_to_cellPos(id: int) -> Vector2i:
-	return 2*Vector2i(id%cardsPerRow, id / cardsPerRow)
+	return Vector2i(id%cardsPerRow, id / cardsPerRow)
+
+func cellPos_to_cardId(cell_pos: Vector2i) -> int:
+	return cell_pos.y*cardsPerRow + cell_pos.x
 
 func card_to_tile(card: Card) -> Vector2i:
 #	return Vector2i(card.value, 2*card.color) # for manual debug: display the card values
 	return Vector2i(card.value, 2*card.color + 1)
+
+func _input(event):
+	if event.is_action_pressed("select_card"):
+		var click_pos: Vector2 = get_global_mouse_position()
+		var cell_pos: Vector2i = local_to_map(click_pos-position)
+		var card_id: int = cellPos_to_cardId(cell_pos)
+		if card_id < 0 or card_id > cards.size() or not cards[card_id]:
+			print("click was not on a card, we ignore it")
+			return
+		cardPicked.emit(cards[card_id])
+		player_picked_card.rpc(card_id)
+
+
+@rpc("any_peer", "call_local", "reliable")
+func player_picked_card(card_id: int) -> void:
+	cards[card_id] = null
+	paint()
--- scripts/Card.gd
+++ scripts/Card.gd
@@ -8,6 +8,9 @@ func _init(color_p, value_p):
 	self.color = color_p
 	self.value = value_p
 
+func _to_string():
+	return "(" + str(value+1) + ", " + str(color) + ")"
+
 static func compare(a: Card, b: Card) -> bool:
 	if a.value < b.value:
 		return true
