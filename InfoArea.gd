c10e2ccabf92d53b0848cc25f370c6e0eae29fca
Guillaume Turri <g.turri@criteo.com>
2024-05-19 21:22:38 +0200

    highlight recent messages
    otherwise it's easy to miss when a message is added

--- InfoArea.gd
+++ InfoArea.gd
@@ -1,9 +1,41 @@
 extends RichTextLabel
 
+@export var timeUntilMessageIsntConsideredRecentAnymore_inSecond = 3
+var allEntries := [] # TODO: create an ad hoc Entry class
+
 func log_info(message: String) -> void:
-	append_text("- " + message + "\n")
+	add_message(message, "info")
 
 func log_warning(message: String) -> void:
-	push_color(Color.YELLOW)
-	log_info(message)
-	pop()
+	add_message(message, "warning")
+
+func add_message(message: String, severity: String) -> void:
+	allEntries.append([add_pre_and_postfix(message), severity, true])
+	print_entries()
+	var currentSize = allEntries.size()
+	get_tree().create_timer(timeUntilMessageIsntConsideredRecentAnymore_inSecond).timeout.connect(func (): mark_message_as_not_recent_anymore(currentSize-1))
+
+func add_pre_and_postfix(message: String) -> String:
+	return "- " + message + "\n"
+
+func mark_message_as_not_recent_anymore(entryIndex: int) -> void:
+	allEntries[entryIndex][2] = false
+	print_entries()
+
+func print_entries() -> void:
+	var allText = ""
+	for entry in allEntries:
+		var message = entry[0]
+		var severity = entry[1]
+		var isRecentMessage: bool = entry[2]
+		var optionalOpeningTags := ""
+		var optionalClosingTags := ""
+		if severity == "warning":
+			optionalOpeningTags += "[color=yellow]"
+			optionalClosingTags += "[/color]"
+		if isRecentMessage:
+			optionalOpeningTags += "[shake]" #TODO: find something more user friendly (see eg https://forum.godotengine.org/t/trying-to-make-the-highlight-of-a-richtextlabel-blink/38042 )
+			optionalClosingTags = "[/shake]" + optionalClosingTags
+		allText += optionalOpeningTags + message + optionalClosingTags
+	set_text(allText)
+
