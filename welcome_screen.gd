1e5c0f638cbc1fbc49b69edd2eb4d1c52f3d1c62
Guillaume Turri <g.turri@criteo.com>
2024-06-29 23:41:05 +0200

    Can connect by pressing "Enter"

--- welcome_screen.gd
+++ welcome_screen.gd
@@ -10,6 +10,7 @@ func _on_host_game_pressed():
 	get_tree().change_scene_to_file("res://lobby.tscn")
 
 func _on_connect_to_pressed():
+	print("going to try to connect")
 	var peer = ENetMultiplayerPeer.new()
 	var error = peer.create_client($Address.get_text(), port)
 	if error:
@@ -19,6 +20,10 @@ func _on_connect_to_pressed():
 	get_tree().get_multiplayer().multiplayer_peer = peer
 	# TODO: print a message saying it's connecting, give the possibility to cancel, ...
 
+func _input(event):
+	if $Address.has_focus() and event.is_action_pressed("submit_form"):
+		get_viewport().set_input_as_handled()
+		_on_connect_to_pressed()
 
 func connected():
 	print("Connected to server")
